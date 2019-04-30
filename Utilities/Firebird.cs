6cec0c91b4a51edbc60b1cea7913e27d6d0aa20d
NEXUS\zur003 <Eric.Zurcher@csiro.au>
2019-04-30 16:01:11 +1000

    Added Firebird implementation for CreateIndex.

--- Utilities/Firebird.cs
+++ Utilities/Firebird.cs
@@ -956,7 +956,24 @@ namespace APSIM.Shared.Utilities
         /// <param name="isUnique">Is the index a primary key?</param>
         public void CreateIndex(string tableName, List<string> colNames, bool isUnique)
         {
+            StringBuilder columnNamesCSV = new StringBuilder();
+            for (int c = 0; c < colNames.Count; c++)
+            {
+                if (columnNamesCSV.Length > 0)
+                    columnNamesCSV.Append(',');
 
+                columnNamesCSV.Append("[");
+                columnNamesCSV.Append(colNames[c]);
+                columnNamesCSV.Append("] ");
+            }
+
+            string uniqueString = null;
+            if (isUnique)
+                uniqueString = "UNIQUE";
+
+            var sql = String.Format("CREATE {0} INDEX [{1}Index] ON [{1}] ({2})",
+                                    uniqueString, tableName, columnNamesCSV.ToString());
+            ExecuteNonQuery(sql);
         }
 
         /// <summary>
