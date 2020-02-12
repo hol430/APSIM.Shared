445eceadbfe67c20baff656167ff9fe5034cafe9
hol430 <drew.holzworth@csiro.au>
2020-02-12 13:03:03 +1000

    Moved Upgrade class from APSIM.Builds into APSIM.Shared
    This class is now required by the APSIM.Registration repo, so I'm moving it here.

---
+++ Web/Upgrade.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSIM.Shared.Web
+{
+    /// <summary>
+    /// An class encapsulating an upgrade 
+    /// </summary>
+    public class Upgrade
+    {
+        /// <summary>
+        /// Release date of the upgrade.
+        /// </summary>
+        public DateTime ReleaseDate { get; set; }
+
+        /// <summary>
+        /// Number/ID of the issue addressed by this upgrade.
+        /// </summary>
+        public int IssueNumber { get; set; }
+
+        /// <summary>
+        /// Title of the issue addressed by this upgrade.
+        /// </summary>
+        public string IssueTitle { get; set; }
+
+        /// <summary>
+        /// URL of the issue addressed by this upgrade.
+        /// </summary>
+        public string IssueURL { get; set; }
+
+        /// <summary>
+        /// URL of the installer for this upgrade.
+        /// </summary>
+        public string ReleaseURL { get; set; }
+
+        /// <summary>
+        /// Issue Number (obsolete).
+        /// </summary>
+        // Leaving this here for compatibility reasons.
+        [Obsolete("Deprecated in favour of IssueNumber.")]
+        public int issueNumber
+        {
+            get
+            {
+                return IssueNumber;
+            }
+            set
+            {
+                IssueNumber = value;
+            }
+        }
+    }
+}
